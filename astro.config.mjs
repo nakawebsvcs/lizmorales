abeaed13caba9369c27e47859cde5f6ed31a8633
Jessica Nakamura <jessinaka@Jessicas-MacBook-Pro.local>
2025-05-14 15:06:05 -1000

    Initial Project Setup

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  site: "https://www.lizmorales.com",
+});
